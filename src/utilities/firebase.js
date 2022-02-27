dae9c37fae498192fc43868f69889ffa6954d5b6
gracewainaina <47871572+gracewainaina@users.noreply.github.com>
2022-02-27 14:49:27 -0600

    fetching data from database

---
+++ src/utilities/firebase.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, onValue, ref, set } from 'firebase/database';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDNWHJ9j4Hw2YVkU799AY6K7ShHUxUp9Wg",
+    authDomain: "scheduler-fbc65.firebaseapp.com",
+    databaseURL: "https://scheduler-fbc65-default-rtdb.firebaseio.com",
+    projectId: "scheduler-fbc65",
+    storageBucket: "scheduler-fbc65.appspot.com",
+    messagingSenderId: "780867691251",
+    appId: "1:780867691251:web:03b4b6f0a32bde86bb4d86",
+    measurementId: "G-C6WXG0E429"
+};
+
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+export const useData = (path, transform) => {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+  
+    useEffect(() => {
+      const dbRef = ref(database, path);
+      const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+      if (devMode) { console.log(`loading ${path}`); }
+      return onValue(dbRef, (snapshot) => {
+        const val = snapshot.val();
+        if (devMode) { console.log(val); }
+        setData(transform ? transform(val) : val);
+        setLoading(false);
+        setError(null);
+      }, (error) => {
+        setData(null);
+        setLoading(false);
+        setError(error);
+      });
+    }, [path, transform]);
+  
+    return [data, loading, error];
+};
+  
